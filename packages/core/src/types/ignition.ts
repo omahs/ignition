405715b79c5db5a7e85130af5aec8ff10038d88b
John Kane <john@kanej.me>
2023-03-23 12:21:19 +0000

    chore: add api-extractor to core
    The devtool `api-extractor` provides linting errors on our tsdocs
    including warning on types that are accessible through the public api
    but not exported.
    
    A command `npm run api-extractor` has been added to core, and this check
    has been included under core's lint command.

---
+++ packages/core/src/types/ignition.ts
@@ -0,0 +1,28 @@
+import { UpdateUiAction } from "../internal/types/deployment";
+import { ICommandJournal } from "../internal/types/journal";
+import { Services } from "../internal/types/services";
+
+/**
+ * The setup options for Ignition
+ *
+ * @internal
+ */
+export interface IgnitionConstructorArgs {
+  /**
+   * An adapter that allows Ignition to communicate with external services
+   * like the target blockchain or local filesystem.
+   */
+  services: Services;
+
+  /**
+   * An optional UI update function that will be invoked with the current
+   * Ignition state on each major state change.
+   */
+  uiRenderer?: UpdateUiAction;
+
+  /**
+   * An optional journal that will be used to store a record of the current
+   * run and to access the history of previous runs.
+   */
+  journal?: ICommandJournal;
+}
