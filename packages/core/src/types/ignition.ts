3400f7f094704bf0959970a2679ca8cb7f4df0ee
Patricio Palladino <email@patriciopalladino.com>
2023-03-30 15:41:42 -0300

    Replace internal types in types/ignition.ts with unknown

--- packages/core/src/types/ignition.ts
+++ packages/core/src/types/ignition.ts
@@ -1,6 +1,27 @@
-import { UpdateUiAction } from "../internal/types/deployment";
-import { ICommandJournal } from "../internal/types/journal";
-import { Services } from "../internal/types/services";
+import type { BigNumber } from "ethers";
+import { Module, ModuleDict } from "./module";
+import { IgnitionPlan } from "./plan";
+
+/**
+ * The type of the services that Ignition uses. Soon to be made private.
+ *
+ * @internal
+ */
+export type ServicesT = unknown;
+
+/**
+ * The type of the CommandJournal that Ignition uses.
+ *
+ * @alpha
+ */
+export type ICommandJournalT = unknown;
+
+/**
+ * The type of a callback to update the UI.
+ *
+ * @alpha
+ */
+export type UpdateUiActionT = unknown;
 
 /**
  * The setup options for Ignition
@@ -12,17 +33,17 @@ export interface IgnitionConstructorArgs {
    * An adapter that allows Ignition to communicate with external services
    * like the target blockchain or local filesystem.
    */
-  services: Services;
+  services: ServicesT;
 
   /**
    * An optional UI update function that will be invoked with the current
    * Ignition state on each major state change.
    */
-  uiRenderer?: UpdateUiAction;
+  uiRenderer?: UpdateUiActionT;
 
   /**
    * An optional journal that will be used to store a record of the current
    * run and to access the history of previous runs.
    */
-  journal?: ICommandJournal;
+  journal?: ICommandJournalT;
 }
